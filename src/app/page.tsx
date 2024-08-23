0a923ca6c431875212edbb0ec2c182fd97442954
Karthik Karavatt <karthikkaravatt@gmail.com>
2024-08-24 00:13:29 +0800

    adjsted cubes in the background and added a change colour on hover effect to the cubes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,40 @@
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import styles from './Home.module.css'
-import RotatingCube from './components/RotatingCube';
+import styles from './Home.module.css';
+import BackgroundCubes from './components/BackgroundCubes'; // Import the BackgroundCubes component
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <BackgroundCubes /> {/* Integrate the background cubes */}
       <div className={styles.headerContainer}>
         <div className={styles.overlay}></div>
-        <h1 style={{ fontSize: '3rem' }}>
-          Karthik Karavatt
-        </h1>
+        <h1 style={{ fontSize: '3rem' }}>Karthik Karavatt</h1>
         <div style={{ marginTop: '0.5rem', display: 'flex', gap: '1rem' }}>
-          <a href="https://github.com/KarthikKaravatt" target="_blank"
-            rel="noopener noreferrer" style={{ marginRight: '1rem' }}>
-            <FontAwesomeIcon icon={faGithub}
-              style={{ width: '2rem', height: '2rem' }} />
+          <a
+            href="https://github.com/KarthikKaravatt"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginRight: '1rem' }}
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{ width: '2rem', height: '2rem' }}
+            />
           </a>
-          <a href="https://www.linkedin.com/in/karthik-karavatt-098261257/"
-            target="_blank" rel="noopener noreferrer"
-            style={{ marginRight: '1rem' }}>
-            <FontAwesomeIcon icon={faLinkedin}
-              style={{ width: '2rem', height: '2rem' }} />
+          <a
+            href="https://www.linkedin.com/in/karthik-karavatt-098261257/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginRight: '1rem' }}
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              style={{ width: '2rem', height: '2rem' }}
+            />
           </a>
         </div>
-      </div>
-      <div style={{ height: '400px', width: '400px', margin: '0 auto' }}>
-        <RotatingCube /> {/* This should render the rotating cube */}
       </div>
     </main>
   );
