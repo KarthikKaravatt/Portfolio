e3ea35790c19a5c1f90d9e46ce98ba6db2bd7396
Karthik Karavatt <karthikkaravatt@gmail.com>
2024-08-24 15:40:00 +0800

    Fixed the bugs with the cubes on mobile and addes some cards

--- src/app/components/BackgroundCubes.tsx
+++ src/app/components/BackgroundCubes.tsx
@@ -1,30 +1,22 @@
 
 'use client';
 
+import { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
 import { Box, Edges } from '@react-three/drei';
 
 const CUBE_SIZE = 1;
-// Minimum distance between cubes to avoid collision
 const MIN_DISTANCE = CUBE_SIZE * 2.5;
 const NUM_CUBES = 100;
-// Increased scene size to place cubes further from the center
 const SCENE_SIZE = 40;
-// Speed of cube rotation
 const ROTATION_SPEED = 0.004;
-// Intensity of ambient light
 const AMBIENT_LIGHT_INTENSITY = 3;
-// Position the camera away from the center
 const CAMERA_POSITION: [number, number, number] = [0, 0, 30];
-// Field of view for the perspective camera
 const FOV = 60;
-// Calculate grid size based on the scene and minimum distance
 const GRID_SIZE = Math.floor(SCENE_SIZE / MIN_DISTANCE);
 
 function SpinningBox({ position }: { position: [number, number, number] }) {
   const cubeRef = useRef<any>();
-  const [hovered, onHover] = useState(false);
 
   useFrame(() => {
     if (cubeRef.current) {
@@ -35,14 +27,11 @@ function SpinningBox({ position }: { position: [number, number, number] }) {
 
   return (
     <group ref={cubeRef} position={position}>
-      <mesh
-        onPointerEnter={() => onHover(true)}
-        onPointerLeave={() => onHover(false)}
-      >
+      <mesh >
         <Box args={[1, 1, 1]}>
           <meshStandardMaterial
             attach="material"
-            color={hovered ? "white" : "black"}
+            color={"black"}
           />
           <Edges color="white" lineWidth={3} />
         </Box>
@@ -52,8 +41,34 @@ function SpinningBox({ position }: { position: [number, number, number] }) {
 }
 
 export default function BackgroundCubes() {
-  // Declare and initialize the grid
-  const grid: boolean[][][] = Array(GRID_SIZE)
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: document.documentElement.scrollHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: document.documentElement.scrollHeight,
+      });
+    };
+
+    // Set initial dimensions
+    handleResize();
+
+    // Update dimensions on window resize or orientation change
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('orientationchange', handleResize);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('orientationchange', handleResize);
+    };
+  }, []);
+
+  const grid = Array(GRID_SIZE)
     .fill(null)
     .map(() =>
       Array(GRID_SIZE)
@@ -61,7 +76,6 @@ export default function BackgroundCubes() {
         .map(() => Array(GRID_SIZE).fill(false))
     );
 
-  // Generate a random grid position in the form of a tuple [x, y, z]
   const generateGridPosition = (): [number, number, number] => {
     const x = Math.floor(Math.random() * GRID_SIZE);
     const y = Math.floor(Math.random() * GRID_SIZE);
@@ -70,12 +84,10 @@ export default function BackgroundCubes() {
     return [x, y, z];
   };
 
-  // Confirm cubes do not overlap
   const isGridPositionValid = (position: [number, number, number]): boolean => {
     return !grid[position[0]][position[1]][position[2]];
   };
 
-  // Convert grid position to world coordinates
   const convertGridPositionToWorldPosition = (
     position: [number, number, number]
   ): [number, number, number] => {
@@ -91,28 +103,29 @@ export default function BackgroundCubes() {
   while (cubePositions.length < NUM_CUBES) {
     const gridPosition = generateGridPosition();
     if (isGridPositionValid(gridPosition)) {
-      grid[gridPosition[0]][gridPosition[1]][gridPosition[2]] = true; // Mark the grid position as occupied
+      grid[gridPosition[0]][gridPosition[1]][gridPosition[2]] = true;
       const worldPosition = convertGridPositionToWorldPosition(gridPosition);
       cubePositions.push(worldPosition);
     }
   }
 
   return (
-    <Canvas
+    <div
       style={{
-        position: 'absolute',
+        position: 'fixed',
         top: 0,
         left: 0,
-        width: '100vw',
-        height: '100vh',
-        zIndex: -1,
+        width: dimensions.width,
+        height: dimensions.height,
+        zIndex: -2,
       }}
-      camera={{ position: CAMERA_POSITION, fov: FOV }}
     >
-      <ambientLight intensity={AMBIENT_LIGHT_INTENSITY} />
-      {cubePositions.map((pos, index) => (
-        <SpinningBox key={index} position={pos} />
-      ))}
-    </Canvas>
+      <Canvas camera={{ position: CAMERA_POSITION, fov: FOV }}>
+        <ambientLight intensity={AMBIENT_LIGHT_INTENSITY} />
+        {cubePositions.map((pos, index) => (
+          <SpinningBox key={index} position={pos} />
+        ))}
+      </Canvas>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,53 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import styles from './Home.module.css';
-import BackgroundCubes from './components/BackgroundCubes'; // Import the BackgroundCubes component
+import BackgroundCubes from './components/BackgroundCubes';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <BackgroundCubes /> {/* Integrate the background cubes */}
-      <div className={styles.headerContainer}>
-        <div className={styles.overlay}></div>
-        <h1 style={{ fontSize: '3rem' }}>Karthik Karavatt</h1>
-        <div style={{ marginTop: '0.5rem', display: 'flex', gap: '1rem' }}>
-          <a
-            href="https://github.com/KarthikKaravatt"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '1rem' }}
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              style={{ width: '2rem', height: '2rem' }}
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/karthik-karavatt-098261257/"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ marginRight: '1rem' }}
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              style={{ width: '2rem', height: '2rem' }}
-            />
-          </a>
+    <main>
+      <div className={styles.mainCard}>
+        <BackgroundCubes /> 
+        <div className={styles.mainCardContent}>
+          <div className={styles.overlay}></div>
+          <h1 style={{ fontSize: '3rem' }}>Karthik Karavatt</h1>
+          <div style={{ marginTop: '0.5rem', display: 'flex', gap: '1rem' }}>
+            <a
+              href="https://github.com/KarthikKaravatt"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginRight: '1rem' }}
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                style={{ width: '2rem', height: '2rem' }}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/karthik-karavatt-098261257/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ marginRight: '1rem' }}
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ width: '2rem', height: '2rem' }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.infoCards}>
+        <div className={styles.infoCardsContent}>
+          <div className={styles.infoCard}>
+            <p> LOL </p>
+          </div>
+          <div className={styles.infoCard}>
+            <p> LOL </p>
+          </div>
+          <div className={styles.infoCard}>
+            <p> LOL </p>
+          </div>
         </div>
       </div>
     </main>
